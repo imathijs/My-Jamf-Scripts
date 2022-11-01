2eab076f8ed41992b34e859bba84cf1e7cad6e61
mathijs.w <mathijs@prowarehouse.nl>
2022-11-01 15:13:06 +0100

    Version 1

---
+++ Support/Jamf self healing.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+## postinstall
+#
+# JAMF BINARY SELF-HEAL
+#
+# Mathijs de Willigen
+# Script is inspirated by modtitan:
+# https://www.modtitan.com/2022/02/jamf-binary-self-heal-with-jamf-api.html
+
+# DEBUG MODE (0 = FALSE / 1 = TRUE)
+DEBUG=1
+
+# FIND INTERNAL SERIALNUMBER
+if [ $DEBUG = 0 ]; then
+	SERIALCOMP=$(ioreg -l | grep IOPlatformSerialNumber | awk '{print $4}' | tr -d '"')
+else 
+	#
+	SERIALCOMP="<serial>"
+fi
+
+# JAMF CLOUD URL
+URL="https://<url>.jamfcloud.com/"
+
+# USERNAME PASSWORD BASE64
+APIHASH="<hash>"
+
+## Retrieve the authToken using the APIHASH
+authToken=$(curl -X POST "${URL}api/v1/auth/token" -H "accept: application/json" -H "Authorization: Basic ${APIHASH}")
+## Retrieve the api_token using the authToken
+api_token=$(/usr/bin/awk -F \" 'NR==2{print $4}' <<< "$authToken" | /usr/bin/xargs)
+
+# GET JAMF COMPUTER ID RECORD
+JAMFID=$(curl -X GET "${URL}/JSSResource/computers/serialnumber/${SERIALCOMP}" -H "accept: application/xml" -H "accept: application/json" -H "Authorization: Bearer $api_token" | xmllint --xpath '/computer/general/id' - | tr '</id>' ''​)
+
+# REDEPLOY IN JAMF PRO
+if [ $DEBUG = 0 ]; then
+	curl -X POST "${URL}api/v1/jamf-management-framework/redeploy/${JAMFID}" -H "accept: application/json" -H "Authorization: Bearer $api_token"
+else
+	echo "curl -X POST ${URL}api/v1/jamf-management-framework/redeploy/${JAMFID}"
+fi
+

3a00bbf4c94b7b06ea312ade73dbbf7f038aab5b
mathijs.w <mathijs@prowarehouse.nl>
2022-11-02 20:19:48 +0100

    Changed result lookup computer id

--- Support/Jamf self healing.sh
+++ Support/Jamf self healing.sh
@@ -25,12 +25,18 @@ URL="https://<url>.jamfcloud.com/"
 APIHASH="<hash>"
 
 ## Retrieve the authToken using the APIHASH
-authToken=$(curl -X POST "${URL}api/v1/auth/token" -H "accept: application/json" -H "Authorization: Basic ${APIHASH}")
+authToken=$(curl -sX POST "${URL}api/v1/auth/token" -H "accept: application/json" -H "Authorization: Basic ${APIHASH}")
 ## Retrieve the api_token using the authToken
 api_token=$(/usr/bin/awk -F \" 'NR==2{print $4}' <<< "$authToken" | /usr/bin/xargs)
 
 # GET JAMF COMPUTER ID RECORD
-JAMFID=$(curl -X GET "${URL}/JSSResource/computers/serialnumber/${SERIALCOMP}" -H "accept: application/xml" -H "accept: application/json" -H "Authorization: Bearer $api_token" | xmllint --xpath '/computer/general/id' - | tr '</id>' ''​)
+JAMFID=$(curl -sX GET "${URL}/JSSResource/computers/serialnumber/${SERIALCOMP}" -H "accept: application/xml" -H "accept: application/json" -H "Authorization: Bearer $api_token" | xmllint --xpath '/computer/general/id' - | tr -d '</id>' ​)
+
+echo "###############################"
+echo "Start Re-deploy ---------------"
+echo "SerialNumber Mac: ${SERIALCOMP}"
+echo "JamfID: ${JAMFID}"
+echo "###############################"
 
 # REDEPLOY IN JAMF PRO
 if [ $DEBUG = 0 ]; then

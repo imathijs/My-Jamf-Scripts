b0af6b16f2c4df897e0ef166f9421f92c7ecc0bf
mathijs.w <mathijs@prowarehouse.nl>
2022-11-09 13:26:24 +0100

    get restricted software list

---
+++ Support/Get restriction software.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# JAMF CLOUD TENANT URL
+URL="https://<url>.jamfcloud.com/"
+
+# USERNAME PASSWORD BASE64
+APIHASH="<hash>"
+
+## Retrieve the authToken using the APIHASH
+authToken=$(curl -sX POST "${URL}api/v1/auth/token" -H "accept: application/json" -H "Authorization: Basic ${APIHASH}")
+## Retrieve the api_token using the authToken
+api_token=$(/usr/bin/awk -F \" 'NR==2{print $4}' <<< "$authToken" | /usr/bin/xargs)
+
+# GET RESTRICTIONS
+curl -sX GET "${URL}JSSResource/restrictedsoftware" -H "accept: application/json" -H "Authorization: Bearer $api_token" | json_pp
+

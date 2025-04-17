1d8c5fedef1631d3926e76c848a7e895183d06d3
Mathijs de Willigen <mathijs@prowarehouse.nl>
2025-04-17 10:44:18 +0200

    sample swiftdialog

---
+++ Swiftdialogs/update message.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Sample SwiftDialog prompt(s)
+
+# Update prompt
+/Library/Application\ Support/Dialog/Dialog.app/Contents/MacOS/Dialog --ontop --mini --title "Upgrade Snoozed" --message "If you decide you'd like to proceed with the upgrade before the snooze completes, please click 'Update Now'." --button1text "Update Later" --button2text "Update Update" --icon SF=laptopcomputer
+
